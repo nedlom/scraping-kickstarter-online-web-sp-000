a69b4723321fd8eeb96ce1e8eff0ae679c2734e8
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-30 17:28:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -13,7 +13,7 @@ require 'pry'
 # image link: project.css("div.project-thumbnail a img").attribute("src").value
 # description: project.css("p.bbcard_blurb").text
 # location: project.css("li a span.location-name").text
-# percent funded: project.css("ul.project-stats li.first.funded strong").text
+# percent funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
