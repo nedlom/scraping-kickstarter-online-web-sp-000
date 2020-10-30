998bea05fccefa84cac5a829d79b1a44ddd65ad7
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-30 17:48:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -23,7 +23,7 @@ def create_project_hash
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
     projects[title.to_sym] = {
-      image_link:,
+      image_link: ,
       description:,
       location:,
       percent_funded:
